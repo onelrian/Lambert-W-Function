ad489de8cee89d8febf1aaff6a61c132e1709d5c
onelrian <onelsob57@gmail.com>
2025-02-22 16:00:15 +0100

    feat: switch input method from prompting to CLI

--- src/input.rs
+++ src/input.rs
@@ -1,11 +1,24 @@
-use std::io;
+use structopt::StructOpt;
+/// CLI Options
+#[derive(StructOpt)]
 
-pub fn input(promt: &str) -> i32 {
-    let mut input = String::new();
-
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed To Read Input");
-
-    input.trim().parse::<i32>().unwrap_or(0)
+pub struct Cli {
+    /// The input value for the Lambert W function
+    #[structopt(parse(try_from_str = parse_f64))]
+    x: f64,
 }
+
+fn parse_f64(src: &str) -> Result<f64, String> {
+    src.parse().map_err(|_| format!("Invalid number: {}", src))
+}
+
+impl Cli {
+    pub fn new() -> Cli {
+        Cli::from_args()
+    }
+
+    pub fn get(&self) -> f64 {
+        self.x
+    }
+}
+
